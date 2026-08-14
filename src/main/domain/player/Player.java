f506c089b95ea81d3d26e273eaa15d89766804e8
LongNightOfSolace2552 <wxyong.nz@gmail.com>
2026-08-14 17:00:34 +1200

    (14/08/2026) [4:59 pm] Completed player class files for Player object and Stats object in domain folder.

--- src/main/domain/player/Player.java
+++ src/main/domain/player/Player.java
@@ -1,6 +1,57 @@
 package main.domain.player;
+import java.util.ArrayList;
+import java.util.List;
+import main.domain.items.Item;
 
 // Player model, including core stats (Strength, Magic, Agility), equipped items, and current node location.
 public class Player {
-
+    private String name;
+    private Stats stats;
+    private String currentNodeName;
+    private final List<Item> inventory = new ArrayList<>();
+    
+    //constructor
+    public Player(String name, Stats stats, String currentNodeName) {
+        this.name = name;
+        this.stats = stats;
+        this.currentNodeName = currentNodeName;
+    }
+    
+    //get method
+    public String getName() {
+        return name;
+    }
+    
+    public Stats getStats() {
+        return stats;
+    }
+    
+    public String getCurrentNodeName() {
+        return currentNodeName;
+    }
+    
+    //set method
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setCurrentNodeName() {
+        this.currentNodeName = currentNodeName;
+    }
+    
+    /*
+    returns a copy of players inventory, so players can't bypass
+    addItem and removeItem to mutate the player's inventory directly
+    */
+    public List<Item> getInvetory() {
+        return new ArrayList<>(inventory);
+    }
+    
+    public void addItem(Item item) {
+        inventory.add(item);
+    }
+    
+    public boolean removeItem(Item item) {
+        return inventory.remove(item);
+    }
 }
