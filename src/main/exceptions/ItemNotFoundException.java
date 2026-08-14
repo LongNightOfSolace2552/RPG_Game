94c6ef1a969d94bbde12bf4e37b3bd9b9d5071e1
LongNightOfSolace2552 <wxyong.nz@gmail.com>
2026-08-14 16:38:12 +1200

    (14/08/2026) Completed Exceptions for error handling.

--- src/main/exceptions/ItemNotFoundException.java
+++ src/main/exceptions/ItemNotFoundException.java
@@ -3,5 +3,7 @@ package main.exceptions;
 // Thrown when a referenced item, enemy, node, or dialogue template ID does
 // not exist in the loaded data.
 public class ItemNotFoundException extends GameException {
-
+    public ItemNotFoundException(String message) {
+        super(message);
+    }
 }
--- src/main/exceptions/SaveDataException.java
+++ src/main/exceptions/SaveDataException.java
@@ -3,5 +3,11 @@ package main.exceptions;
 // Thrown when the player's save file is missing, unreadable, or corrupted,
 // or when writing save data fails.
 public class SaveDataException extends GameException {
-
+    public SaveDataException(String message) {
+        super(message);
+    }
+    
+    public SaveDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
