4251a64c6af108f4a9429997b2f9e372af537f6a
LongNightOfSolace2552 <wxyong.nz@gmail.com>
2026-08-14 17:18:35 +1200

    (14/08/2026) [5:05 pm] Completed FileWriterUtil. Work In Progress: PlayerFileRepository

--- src/main/persistence/PlayerFileRepository.java
+++ src/main/persistence/PlayerFileRepository.java
@@ -1,6 +1,22 @@
 package main.persistence;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import main.domain.player.Player;
+import main.domain.player.Stats;
+import main.exceptions.DataLoadException;
+import main.exceptions.SaveDataException;
+
 // Loads and saves player data.
 public class PlayerFileRepository {
-
+    //create constants used internally by this class
+    private static final String KEY_NAME = "name";
+    private static final String KEY_STRENGTH = "strength";
+    private static final String KEY_MAGIC = "magic";
+    private static final String KEY_AGILITY = "agility";
+    private static final String KEY_NODE = "currentNode";
+    
+    
 }
