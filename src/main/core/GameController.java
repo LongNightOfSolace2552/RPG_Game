b233e1e963d65419b866656c64258bcf7072b36f
wxyon <wxyong.nz@gmail.com>
2026-08-16 13:50:50 +1200

    (16/08/2026) Completed SaveManager with comments of relation.

--- src/main/core/GameController.java
+++ src/main/core/GameController.java
@@ -1,6 +1,19 @@
 package main.core;
 
+import main.cui.ConsoleRenderer;
+import main.domain.player.Player;
+import main.exceptions.SaveDataException;
+
 // Orchestrates services and CUI rendering in response to player actions.
 public class GameController {
-
+    private final ConsoleRenderer consoleRenderer;
+    private final SaveManager saveManager;
+    
+    //constructor
+    public GameController(ConsoleRenderer consoleRenderer, SaveManager saveManager) {
+        this.consoleRenderer = consoleRenderer;
+        this.saveManager = saveManager;
+    }
+    
+    
 }
